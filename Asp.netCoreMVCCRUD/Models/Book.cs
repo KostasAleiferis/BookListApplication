a7b6b8cb0aaec7c7b60a8b0dde127984cac47ef7
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2020-10-03 01:15:14 +0200

    Add more Required DataAnnotations and the regex for the ISBN

--- Asp.netCoreMVCCRUD/Models/Book.cs
+++ Asp.netCoreMVCCRUD/Models/Book.cs
@@ -4,13 +4,15 @@ namespace Asp.netCoreMVCCRUD.Models
 {
     public class Book
     {
-       [Key]
+        [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Author{ get; set; }
-
+        [Required]
+        [RegularExpression(@"^(?:ISBN(?:-1[03])?:?\ )?(?=[0-9X]{10}$|(?=(?:[0-9]+[-\ ]){3})[-\ 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[-\ ]){4})[-\ 0-9]{17}$)(?:97[89][-\ ]?)?[0-9]{1,5}[-\ ]?[0-9]+[-\ ]?[0-9]+[-\ ]?[0-9X]$",
+        ErrorMessage = "ISBN not valid")]
         public string ISBN{ get; set; }
     }
 }
